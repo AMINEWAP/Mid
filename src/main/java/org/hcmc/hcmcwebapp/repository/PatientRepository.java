41959a4b749412c4189794e69aac52d01ed46bcd
AMINEWAP <100165297+AMINEWAP@users.noreply.github.com>
2022-04-11 12:03:28 -0500

    Delete src directory

--- src/main/java/org/hcmc/hcmcwebapp/repository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package org.hcmc.hcmcwebapp.repository;
-
-import org.hcmc.hcmcwebapp.model.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
